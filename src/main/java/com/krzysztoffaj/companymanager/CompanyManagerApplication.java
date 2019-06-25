8e491b717409a727aa0208a3f532c61a7d40ab03
Krzysztof Faj <krzysztof.faj@gmail.com>
2019-06-26 01:26:40 +0200

    Successfully run first Spring boot application

--- src/main/java/com/krzysztoffaj/companymanager/CompanyManagerApplication.java
+++ src/main/java/com/krzysztoffaj/companymanager/CompanyManagerApplication.java
@@ -1,13 +1,17 @@
 package com.krzysztoffaj.companymanager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        ErrorMvcAutoConfiguration.class
+})
 public class CompanyManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyManagerApplication.class, args);
 	}
-
 }
