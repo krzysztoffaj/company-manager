d81c9f08375790a96d9acb18352cd49aa723cfaa
Krzysztof Faj <krzysztof.faj@gmail.com>
2019-07-01 01:58:01 +0200

    Handled position and supervisor dropdowns and team multiple select

--- src/main/java/com/krzysztoffaj/companymanager/controllers/AddNewEmployeeController.java
+++ src/main/java/com/krzysztoffaj/companymanager/controllers/AddNewEmployeeController.java
@@ -1,15 +1,31 @@
 package com.krzysztoffaj.companymanager.controllers;
 
+import com.krzysztoffaj.companymanager.entities.Employee;
+import com.krzysztoffaj.companymanager.entities.Team;
+import com.krzysztoffaj.companymanager.services.EmployeeService;
+import com.krzysztoffaj.companymanager.services.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class AddNewEmployeeController {
 
+    @Autowired
+    private EmployeeService employeeService;
+    @Autowired
+    private TeamService teamService;
+
     @GetMapping("/addnewemployee")
     public String addNewEmployeeInit(Model model) {
+        final List<Employee> allEmployees = employeeService.getAll();
+        final List<Team> allTeams = teamService.getAll();
+        model.addAttribute("allEmployees", allEmployees);
+        model.addAttribute("allTeams", allTeams);
 
         return "addnewemployee";
     }
