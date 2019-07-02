16af61d21d83f0f4761a6d84ab7b4fd063c4bb69
Krzysztof Faj <krzysztof.faj@gmail.com>
2019-07-03 00:26:10 +0200

    Handled url redirection when clicking on editing employee in main view

--- src/main/java/com/krzysztoffaj/companymanager/controllers/AddNewEmployeeController.java
+++ src/main/java/com/krzysztoffaj/companymanager/controllers/AddNewEmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -47,8 +48,8 @@ public class AddNewEmployeeController {
         return "search";
     }
 
-    @GetMapping("/editemployee")
-    public String editEmployeeInit(@RequestParam("employeeId") Integer employeeId,
+    @GetMapping("/editemployee/{employeeId}")
+    public String editEmployeeInit(@PathVariable("employeeId") Integer employeeId,
                                    Model model) {
         final List<Employee> allEmployees = employeeService.getAll();
         final List<Team> allTeams = teamService.getAll();
