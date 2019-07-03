8a7c1e49e5b7ce59f18ff5b3498c11f3a1c89921
Krzysztof Faj <krzysztof.faj@gmail.com>
2019-07-03 14:20:21 +0200

    Implemented editing an employee by proper redirection in script

--- src/main/resources/static/js/addnewemployee.js
+++ src/main/resources/static/js/addnewemployee.js
@@ -1,26 +1,29 @@
-$(document).ready(function() {
-    $("#employeesubmit").click(function() {
-        var firstName = $('#firstName').val();
-        var lastName = $('#lastName').val();
-        var position = $('#position').val();
-        var salary = $('#salary').val();
-        var supervisorId = $('#supervisorId').val();
-        var teamIds = $('#teamIds').val();
+function submitemployee(employeeId) {
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var position = $('#position').val();
+    var salary = $('#salary').val();
+    var supervisorId = $('#supervisorId').val();
+    var teamIds = $('#teamIds').val();
 
-//        $.ajax({
-//            url: "/addnewemployeesubmit?firstName=" + firstName + "&lastName=" + lastName + "&position=" + position +
-//                                       "&salary=" + salary + "&supervisorId=" + supervisorId + "&teamIds=" + teamIds,
-//            method: 'GET',
-//            success: function(result){
-//                alert("Success!");
-//                window.location.replace("/search");
-//        }});
+
+    if (employeeId === "null") {
         $.ajax({
-            url: "/editemployeesubmit?position=" + position + "&salary=" + salary + "&supervisorId=" + supervisorId + "&teamIds=" + teamIds,
+            url: "/addnewemployeesubmit?firstName=" + firstName + "&lastName=" + lastName + "&position=" + position +
+                                       "&salary=" + salary + "&supervisorId=" + supervisorId + "&teamIds=" + teamIds,
             method: 'GET',
             success: function(result){
                 alert("Success!");
                 window.location.replace("/search");
         }});
-    });
-});
+    } else {
+        $.ajax({
+            url: "/editemployeesubmit?employeeId=" + employeeId + "&position=" + position + "&salary=" + salary +
+                                    "&supervisorId=" + supervisorId + "&teamIds=" + teamIds,
+            method: 'GET',
+            success: function(result){
+                alert("Success!");
+                window.location.replace("/search");
+        }});
+    }
+}
