0342f9002fe40dd52f6581d5586366508af20c0f
Krzysztof Faj <krzysztof.faj@gmail.com>
2019-07-03 15:01:04 +0200

    Made raw teams view

---
+++ src/main/java/com/krzysztoffaj/companymanager/controllers/BrowseTeamsController.java
@@ -0,0 +1,31 @@
+package com.krzysztoffaj.companymanager.controllers;
+
+import com.krzysztoffaj.companymanager.entities.Employee;
+import com.krzysztoffaj.companymanager.entities.Team;
+import com.krzysztoffaj.companymanager.services.EmployeeService;
+import com.krzysztoffaj.companymanager.services.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BrowseTeamsController {
+
+    @Autowired
+    private EmployeeService employeeService;
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping("/teams")
+    public String browseTeamsInit(Model model) {
+        final List<Team> allTeams = teamService.getAll();
+        final List<Employee> allEmployees = employeeService.getAll();
+        model.addAttribute("allTeams", allTeams);
+        model.addAttribute("allEmployees", allEmployees);
+
+        return "teams";
+    }
+}
