9faaf2a4bc7412f6086e88cffb1166c05fb2cee3
Krzysztof Faj <krzysztof.faj@nexio.pl>
2020-12-30 15:55:10 +0100

    Greatly refactored EmployeePosition

--- backend/src/main/java/com/krzysztoffaj/companymanager/model/domain/enums/EmployeePosition.java
+++ backend/src/main/java/com/krzysztoffaj/companymanager/model/domain/enums/EmployeePosition.java
@@ -1,11 +1,29 @@
 package com.krzysztoffaj.companymanager.model.domain.enums;
 
 public enum EmployeePosition {
-    PM,
-    PO,
-    Scrummaster,
-    Developer,
-    DevOps,
-    Tester,
-    Analyst
+
+    PROJECT_MANAGER("Project manager"),
+    PRODUCT_OWNER("Product owner"),
+    SCRUM_MASTER("Scrum master"),
+    DEVELOPER("Developer"),
+    DEVOPS_ENGINEER("DevOps Engineer"),
+    TESTER("Tester"),
+    ANALYST("Analyst");
+
+    private final String name;
+
+    EmployeePosition(String name) {
+        this.name = name;
+    }
+
+    public static EmployeePosition getStatusByName(String name) {
+        for (EmployeePosition position : values()) {
+            if (position.name.equals(name)) {
+                return position;
+            }
+        }
+        throw new IllegalArgumentException(name);
+    }
+
+
 }
