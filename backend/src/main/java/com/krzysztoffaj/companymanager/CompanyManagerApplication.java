fdadd617acf43fe1c8ccc741515e414fa6c95cb1
Krzysztof Faj <krzysztof.faj@nexio.pl>
2020-12-30 11:55:03 +0100

    Very important commit

--- backend/src/main/java/com/krzysztoffaj/companymanager/CompanyManagerApplication.java
+++ backend/src/main/java/com/krzysztoffaj/companymanager/CompanyManagerApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class CompanyManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyManagerApplication.class, args);
 	}
+
 }
